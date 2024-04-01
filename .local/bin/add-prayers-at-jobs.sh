acfd859fe830d83920a88a442f1e08fece961b8e
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2024-04-01 14:58:34 +0200

    refactor

--- .local/bin/add-prayers-at-jobs.sh
+++ .local/bin/add-prayers-at-jobs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-prayers="$HOME/.local/share/prayers.json"
+prayers=("Fajr" "Dhuhr" "Asr" "Maghrib" "Isha")
+prayers_json="$HOME/.local/share/prayers.json"
 kill_cmd="kill -1 \$(ps aux | grep qatami | awk 'FNR==1{print \$2}')"
 
 # WARNING: THIS SCRIPTS REMOVES ALL JOBS IN QUEUE "P" SCHEDULED USING AT
@@ -13,15 +14,8 @@ if [[ "$(at -q p -l | wc -l)" != "0" ]]; then
 fi
 
 day_idx=$(($(date +%-d) - 1))
-
-declare -A ptimes
-ptimes["Fajr"]=$(date -d "$(jq -r ".data[$day_idx].timings.Fajr" "$prayers")" '+%H:%M %F')
-ptimes["Dhuhr"]=$(date -d "$(jq -r ".data[$day_idx].timings.Dhuhr" "$prayers")" '+%H:%M %F')
-ptimes["Asr"]=$(date -d "$(jq -r ".data[$day_idx].timings.Asr" "$prayers")" '+%H:%M %F')
-ptimes["Maghrib"]=$(date -d "$(jq -r ".data[$day_idx].timings.Maghrib" "$prayers")" '+%H:%M %F')
-ptimes["Isha"]=$(date -d "$(jq -r ".data[$day_idx].timings.Isha" "$prayers")" '+%H:%M %F')
-
-for prayer in "${!ptimes[@]}"; do
+for prayer in "${prayers[@]}"; do
 	echo "-- creating at job for $prayer prayer"
-	printf '[ "$(dunstify --icon="clock-applet-symbolic" --action="Reply,reply" "Prayer Times" "It is time for %s prayer 🕌" -t 30000)" = "2" ] && %s' "$prayer" "$kill_cmd" | at -q p "${ptimes[$prayer]}"
+	ptime=$(date -d "$(jq -r ".data[$day_idx].timings.$prayer" "$prayers_json")" '+%H:%M %F')
+	printf '[ "$(dunstify --icon="clock-applet-symbolic" --action="Reply,reply" "Prayer Times" "It is time for %s prayer 🕌" -t 30000)" = "2" ] && %s' "$prayer" "$kill_cmd" | at -q p "$ptime"
 done
